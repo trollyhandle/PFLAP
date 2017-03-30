182b47436e5095cd72064b31a100475645ca47d3
Tyler Holland <hollandt@sonoma.edu>
2017-03-30 12:24:19 -0700

    initial commit

---
+++ DFA.py
@@ -0,0 +1,3 @@
+
+class DFA:
+    pass
---
+++ Main.py
@@ -0,0 +1,18 @@
+
+# PFLAP
+# Python Finite Automata Program
+# Tyler Holland, Gaybi Igno
+
+# Allows creation, conversion, and manipulation of DFAs and NFAs
+# Operations allowed:
+#   Create, edit NFA/DFA
+#   Simulate inputs
+#   Convert NFA to DFA
+#   On two N/DFA inputs: union, intersect, difference, test equivalence
+
+
+def main():
+    print('yay pflap')
+
+
+main()
