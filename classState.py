37303235446f04f87d63bebb492aa33758f8bb2e
gaybiigno <igno@sonoma.edu>
2017-04-06 13:36:00 -0700

    cleaned up classState

--- classState.py
+++ classState.py
@@ -12,6 +12,7 @@ class State:
     #   final : (bool) this state is an accepting state (initial tracked by FA)
     #   center : Point(x, y) of which the state is centered
     #   name : the name for this state. defaults to q0, q1, ... qn
+    #   label : textbox displaying name of state on circle
     #   transitions : list of transitions going in or out of this state
 
     def __init__(self, center, transitions, circle, name):
@@ -28,9 +29,6 @@ class State:
         print("circle: ", self.circle)
         print("number of trans: ", len(self.transitions))
 
-    def contain(self, point):
-        return self.circle.contains(point)
-
     def add_transition(self, line):
         line.setArrow("last")
         self.transitions.append(line)
@@ -44,54 +42,6 @@ class State:
         for i in range(len(self.transitions)):
             self.transitions[i].undraw()
 
-    """
-    # Updates and redraws connected transitions
-    def redraw(self, index, new):
-        first, second = self.transitions[index].getP1(), self.transitions[index].getP2()
-        if (self.circle.contains(first)):
-            first = new
-        if (self.circle.contains(second)):
-            second = new
-
-        toFix = []
-        for q in reversed(states):
-            if self != q:
-                if self.transitions[index] in q.transitions:
-                    q.transitions.remove(self.transitions[index])
-                    toFix.append(q)
-
-        self.transitions[index].undraw()
-        self.transitions[index] = Line(first, second)
-        self.transitions[index].setArrow("last")
-        for i in range(len(toFix)):
-            toFix[i].add_transition(self.transitions[index])
-    """
-
     def drawAll(self, win):
         for i in range(len(self.transitions)):
             self.transitions[i].draw(win)
-
-
-class States:
-    gui_size = 20
-
-    # Class variables:
-    #   circ : the circle object graphically representing this state
-    #   final : (bool) this state is an accepting state (initial tracked by FA)
-    #
-    #   name: the name for this state. defaults to q0, q1, ... qn
-    #   labels[]: list of labels for this state. no effect on DFA
-    #   trans_in[] : list of transitions originating from this state
-    #   trans_out[] : list of transitions leading to this state
-
-    def __init__(self, centerpoint):
-        self.circ = Circle(centerpoint, self.gui_size)
-        self.transitions = []
-        self.final = False
-
-    def __str__(self):
-        pass
-
-
-
-
