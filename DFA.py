d0861284b6fca061633acff66483861b26f781b8
Tyler Holland <trollyhandle@gmail.com>
2017-04-17 20:53:28 -0700

    dfa generator works!

--- DFA.py
+++ DFA.py
@@ -1,71 +1,105 @@
 import json
-import classState as State
+from collections import deque
 from graphics import *
 
-# combine this class with State?
-class Node:
-    # vars
-    # state_object - State graphically representing this node
-    # transitions - list of outward transitions
-    #   transition list - (dest, value) pairs
-    # id - identifier for this state. not the same as state name
-    def __init__(self, id, gui=None):
-        self.id = id
-        self.state_object = gui
-        self.transitions = {}
 
-    # add transition from this node to dest node
-    def add_transition_to(self, dest, transit):
-        self.transitions[transit] = dest.id
+class State:
 
-    # assign a State object to this node
-    def set_graphic(self, gui):
-        self.state_object = gui
+    def __init__(self, name, is_final=False):
+        self.name = name
+        self.transitions = []
+        self.final = is_final
 
-    def has_graphic(self):
-        return self.state_object is not None
+    def __str__(self):
+        return '"'+self.name+'" -> '+str(self.transitions)+(' final'if self.final else'')
 
+    def add_transition(self, char, ident):
+        # add transition on char to state#ident
+        self.transitions.append((char, ident))
 
-class NodeJSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if o.has_graphic():
-            return [o.id, o.state_object.name, o.transitions]
-        else:
-            return [o.id, None, o.transitions]
+    def dump(self):
+        class StateEncoder(json.JSONEncoder):
+            def default(self, o):
+                if o.has_graphic():
+                    return [o.id, o.state_object.name, o.transitions]
+                else:
+                    return [o.id, None, o.transitions]
+        return json.dumps(self, cls=StateEncoder)
 
 
 class DFA:
     # vars
-    # node_list - list of all nodes in this DFA
-    #
+    # nodes {} - dict of nodes in this dfa
+    # initial - initial node of the FA
+    # final {} - set of final/accepting nodes in the dfa
+
     def __init__(self):
-        self.node_list = []
+        self.nodes = {}
+        self.initial = None
+        self.final = set()
 
     def __str__(self):
-        return ''
+        return 'DFA containing {0} nodes'.format(len(self.nodes))
 
-    def add_node(self):
-        pass
+    def get_node_by_id(self, ident):
+        return self.nodes[ident]
+
+    def dumps(self):
+        class DFAEncoder(json.JSONEncoder):
+            def default(self, o):
+                return [o.get_node_by_id(key) for key in o.nodes ]
+        return json.dumps(self, cls=DFAEncoder)
+
+    def print(self):
+        print('initial state id:', self.initial, ' (name: "'+self.nodes[self.initial].name+'")')
+        for key in self.nodes.keys():
+            print(key, ':', self.nodes[key])
+
+    @staticmethod
+    def generate(alphabet, transition_fn, initial, accept_fn):
+        # make queue for uninitialized states; add initial
+        new_states = deque()
+        new_states.append(initial)
+        ready_states = {initial: 0}
+        # make new DFA
+        dfa = DFA()
+        dfa.nodes[0] = State(initial)
+        dfa.initial = 0
+        # for each state in q:
+        while len(new_states) > 0:
+            state = new_states.popleft()
+            for alpha in alphabet:
+                to_state = transition_fn(state, alpha)
+                if to_state not in ready_states:
+                    # trace a new state, add to DFA
+                    new_states.append(to_state)
+                    ready_states[to_state] = len(dfa.nodes)
+                    dfa.nodes[len(dfa.nodes)] = State(to_state, is_final=accept_fn(to_state))
+                # create the transition
+                dfa.nodes[ready_states[state]].add_transition(alpha, ready_states[to_state])
+        return dfa
 
 
 def main():
-    print('dfa test')
+    print('dfa test\n')
 
-    dfa = {}
-    dfa[0] = Node(0)
-    dfa[0].add_transition_to(Node(1), 'x')
-    dfa[0].set_graphic(State.State(Point(0,0), [], None, 'test'))
+    alphabet = ['a', 'b']
+    strlen = 3
+    transition_fn = lambda x, a: x+a if len(x) < strlen else x[1:]+a
+    accept_fn = lambda x: len(x) == strlen and (x.find('a') == -1 or x.find('b') == -1)
+    dfa = DFA.generate(alphabet, transition_fn, '', accept_fn)
+    dfa.print()
+    # dfa[0] = State(0)
+    # dfa[0].add_transition_to(State(1), 'x')
+    # dfa[0].set_graphic(State.State(Point(0,0), [], None, 'test'))
 
-    test = json.dumps(dfa[0], cls=NodeJSONEncoder)
-    print(test)
-
+    # test = dfa.dumps()
+    # print(test)
 
     # with open('trans_ll.txt') as f:
     #     for line in f.readlines():
     #         Node.construct(line.strip())
     #         print()
-
-
 
 
 main()
